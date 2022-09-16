f895640a0eb850817408f4c7bf34d9b0f6103833
mostafa <mostafameerzad@gmail.com>
2022-09-16 10:13:43 +0430

    complete destination-page structure

---
+++ src/pages/destination/DestinationPageStructure.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function DestinationPageStructure({ name, info, img, distance, time }) {
+  return (
+    <div className="destination">
+      <section className="destination__img-container">
+        <img src={img} alt={name} className="destination__img" />
+      </section>
+
+      <section className="destination__content-container">
+        <h1 className="destination__title">
+          {name}
+        </h1>
+        <p className="destination__info">
+          {info}
+        </p>
+        <hr className="destination__line-break" />
+        <div className="destination__travel-info-container">
+          <div className="destination__info-container">
+            <span className="destination__travel-info">avg. Distance</span>
+            <span className="destination__distance">
+              {distance}
+            </span>
+          </div>
+          <div className="destination__info-container">
+            <span className="destination__travel-info">est. travel time</span>
+            <span className="destination__time">
+              {time}
+            </span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default DestinationPageStructure;
