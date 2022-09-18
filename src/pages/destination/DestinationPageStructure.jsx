57665b8315eeeae75640778f228de84be516e616
mostafa <mostafameerzad@gmail.com>
2022-09-18 12:06:04 +0430

    change page structure to return a react-fragment

--- src/pages/destination/DestinationPageStructure.jsx
+++ src/pages/destination/DestinationPageStructure.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 function DestinationPageStructure({ name, info, img, distance, time }) {
   return (
-    <div className="destination ">
+    <>
+    {/* // <div className="destination "> */}
       <section className="destination__img-container">
         <img src={img} alt={name} className="destination__img" />
       </section>
 
       <section className="destination__content-container">
-        <h1 className="destination__title page-title">
+        <h1 className="destination__name page-title">
           {name}
         </h1>
         <p className="destination__info body-text">
@@ -18,19 +19,20 @@ function DestinationPageStructure({ name, info, img, distance, time }) {
         <div className="destination__travel-info-container">
           <div className="destination__info-container">
             <span className="destination__travel-info">avg. Distance</span>
-            <span className="destination__distance">
+            <span className="destination__travel-data">
               {distance}
             </span>
           </div>
           <div className="destination__info-container">
             <span className="destination__travel-info">est. travel time</span>
-            <span className="destination__time">
+            <span className="destination__travel-data">
               {time}
             </span>
           </div>
         </div>
       </section>
-    </div>
+    {/* // </div> */}
+    </>
   );
 }
 
--- src/pages/technology/TechnologyPageStructure.jsx
+++ src/pages/technology/TechnologyPageStructure.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 function TechnologyPageStructure({title, info, portsImg, landsImg}) {
   return (
-    <div className='technology'>
-    <section className="technology__img-section">
+    <>
+    <section className="technology__img-container">
         <img className='technology__lands-img' src={landsImg} alt={title} />
         <img className='technology__ports-img' src={portsImg} alt={title} />
     </section>
-    <section className="technology__content-section">
+    <section className="technology__content-container">
     <p className="body-text">THE TERMINOLOGY...</p>
 <h1 className='technology__title'>{title}</h1>
 <p className="technology__content body-text">{info}</p>
     </section>
 
-    </div>
+    </>
   )
 }
 
